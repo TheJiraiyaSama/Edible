488f4d3357d06b1ac12aa09d4c7a58e6ae02679b
TheJiraiyaSama <rohitkane001@gmail.com>
2024-03-21 19:26:46 +0530

    Custom Button Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:edible/CButton.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xff99A78E),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color(0xff99B79E),
-                Color(0xff99B87E),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Welcome To EcoBytes",
-              style: TextStyle(
-                  fontSize: 40, fontFamily: 'Sans-Serif', color: Colors.white),
-            ),
-          ),
-        ),
+        backgroundColor: Color(0xff99A78E),
+        body: CButton(),
       ),
     ),
   );
+}
+
+class Basic extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xff99B79E),
+            Color(0xff99B87E),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Welcome To EcoBytes",
+          style: TextStyle(
+              fontSize: 28, fontFamily: 'Hanken Grotesk', color: Colors.black),
+        ),
+      ),
+    );
+  }
 }
